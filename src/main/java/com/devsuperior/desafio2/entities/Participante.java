0e0a5f3900d13e5f9dbae9edb034e564a507aaa1
Lucas Barros Bonine <lucasbonine@live.com>
2025-09-09 08:44:34 -0300

    criação entidades

---
+++ src/main/java/com/devsuperior/desafio2/entities/Participante.java
@@ -0,0 +1,15 @@
+package com.devsuperior.desafio2.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_participante")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Participante {
+}
